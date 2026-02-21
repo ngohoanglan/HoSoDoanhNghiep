5c9bc6a2520c2ff3fbfa58363d772e405796eb8a
ngohoanglan <ngohoanglan@gmail.com>
2026-02-21 21:48:19 +0700

    ...

---
+++ DKKDApp/BusinessLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKKDApp
+{
+    public class BusinessLine
+    {
+        public int Stt { get; set; }
+        public string Code { get; set; } = "";
+        public string Name { get; set; } = "";
+        public bool IsMain { get; set; }
+    }
+
+}
